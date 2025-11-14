afbbf669c16012445605a25448c39eb5e87e79f9
leejinuuuu <jinw810@gmail.com>
2025-11-14 18:27:27 +0900

    251114

--- src/BJ1.java
+++
@@ -1,5 +0,0 @@
-public class BJ1 {
-    public static void main(String[] args) throws Exception{
-        System.out.println("aaa");
-    }
-}
---
+++ src/BJ_1_5_1008.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1_5_1008 {
+        public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Double a = Double.parseDouble(st.nextToken());
+        Double b = Double.parseDouble(st.nextToken());
+        
+        System.out.println(a/b);
+    }
+}
