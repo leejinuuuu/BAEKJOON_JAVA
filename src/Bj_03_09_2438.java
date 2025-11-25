de7cba6a17ef5ed8fcbb3c994862ca6fe275aa8c
leejinuuuu <jinw810@gmail.com>
2025-11-26 00:31:41 +0900

    251126

--- src/Bj_03_09_2438.java
+++ src/Bj_03_09_2438.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Bj_03_09_2438 {
+public class BJ_03_09_2438 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
