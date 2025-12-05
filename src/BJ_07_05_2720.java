75fe36ac83d96fad83e6bcefe2e87d93ea62d42d
leejinuuuu <jinw810@gmail.com>
2025-12-05 16:20:59 +0900

    251205

---
+++ src/BJ_07_05_2720.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BJ_07_05_2720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        int[][] result = new int[t][4];
+
+        for(int i=0; i<t; i++) {
+            int money = sc.nextInt();
+
+            result[i][0] = (money / 25);
+            result[i][1] = ((money % 25) / 10);
+            result[i][2] = ((money % 25 % 10) / 5);
+            result[i][3] = ((money % 25 % 10 % 5) / 1);
+        }
+
+        for(int i=0; i<t; i++) {
+            for(int j=0; j<result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/BJ_07_06_2903.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BJ_07_06_2903 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int dis = 1;
+        int result = 3;
+        for(int i=1; i<n; i++) {
+            dis *= 2;
+            result += dis;
+        }
+
+        System.out.println(result*result);
+    }
+}
