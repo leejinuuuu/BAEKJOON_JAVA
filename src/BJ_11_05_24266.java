1a1b65492f9869dbb8d8e285a0a51a35fb4e1ff8
leejinuuuu <jinw810@gmail.com>
2025-12-20 16:31:23 +0900

    251220

---
+++ src/BJ_11_05_24266.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BJ_11_05_24266 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Long n = sc.nextLong();
+
+        System.out.println(n*n*n);
+        System.out.println("3");
+
+        sc.close();
+    }
+}
